9fd472e93038b1fe78fdaed000447c6065ea5b0c
alordash <alordash@mail.ru>
2023-11-06 20:42:31 +0300

    Restructure, added router

---
+++ game-queue-react-app/src/components/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+
+export const IndexComponent: FC = () => 
+    <>Well, hello there
+    <br/>
+    <a href="./demo">Open demo</a></>;
---
+++ game-queue-react-app/src/router/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { IndexComponent } from '../components';
+import { DemoComponent } from '../components/demo';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <IndexComponent/>
+    },
+    {
+        path: '/demo',
+        element: <DemoComponent/>
+    }
+]);
