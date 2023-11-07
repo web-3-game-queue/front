527c772ac8101a1baf59a33c14fba1b9d6427c69
alordash <alordash@mail.ru>
2023-11-08 01:23:40 +0300

    Added breadcrumbs

--- game-queue-react-app/src/Components/Basic/Header.tsx
+++ game-queue-react-app/src/Components/Basic/Header.tsx
@@ -1,31 +1,52 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-export const HeaderComponent: FC = () => (
-    <header>
-        <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
-            <div className="container">
-                <Link className="navbar-brand" to="./">
-                    Game queue
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-                    <ul className="navbar-nav flex-grow-1">
-                        <li className="nav-item">
-                            <Link className="nav-link text-dark" to="./maps">
-                                Карты
-                            </Link>
+export const HeaderComponent: FC = () => {
+    const location = useLocation();
+    const paths = location.pathname.split('/').filter((x) => x !== '');
+    const lastPath = paths.splice(paths.length - 1);
+    return (
+        <header>
+            <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item active" aria-current="page">
+                            <Link to="/">Home</Link>
                         </li>
-                        {/* <li className="nav-item">
+                        {paths.map((path) => (
+                            <li className="breadcrumb-item" aria-current="page">
+                                <Link to={path}>{path}</Link>
+                            </li>
+                        ))}
+                        <li className="breadcrumb-item active" aria-current="page">
+                            {lastPath}
+                        </li>
+                    </ol>
+                </nav>
+                <br />
+                <div className="container">
+                    <Link className="navbar-brand" to="./">
+                        Game queue
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                        <ul className="navbar-nav flex-grow-1">
+                            <li className="nav-item">
+                                <Link className="nav-link text-dark" to="./maps">
+                                    Карты
+                                </Link>
+                            </li>
+                            {/* <li className="nav-item">
                             <Link className="nav-link text-dark" to="./createUser">
                                 Создать пользователя
                             </Link>
                         </li> */}
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </header>
-);
+            </nav>
+        </header>
+    );
+};
