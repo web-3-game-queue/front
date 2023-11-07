9ff672720184fd6ef5cbd7eb2dcae790a0054b7f
alordash <alordash@mail.ru>
2023-11-07 14:30:05 +0300

    Adding map filters

--- game-queue-react-app/src/Components/Maps/MapCard/MapCard.tsx
+++ game-queue-react-app/src/Components/Maps/MapCard/MapCard.tsx
@@ -1,19 +1,10 @@
 import { FC } from 'react';
 import { Map, MapToString } from '../../../Core/Models/Map';
 import { StaticDataAPI } from '../../../Core/APIs/StaticDataAPI';
-// import { MapStatus } from '../../../Core/Models/MapStatus';
 import { Link } from 'react-router-dom';
 
 export const MapCardComponent: FC<Map> = (map) => {
     const coverImageUrl = StaticDataAPI.FormMapCoverUrl(map);
-    // const controls =
-    //     map.mapStatus == MapStatus.Deleted ? (
-    //         <h5>Недоступна.</h5>
-    //     ) : (
-    //         <form method="post">
-    //             <button className="btn btn-danger">Удалить</button>
-    //         </form>
-    //     );
     return (
         <div className="col mb-3 mb-sm-0 p-0 p-xl-3" key={map.id}>
             <div className="card" style={{ width: 'auto' }}>
--- game-queue-react-app/src/Components/Maps/MapsPage.tsx
+++ game-queue-react-app/src/Components/Maps/MapsPage.tsx
@@ -16,5 +16,31 @@ export const MapsPageComponent: FC = () => {
     if (!maps) {
         return <LoadingIndicator />;
     }
-    return <MapListComponent maps={maps} />;
+    return (
+        <div className="container" style={{ display: 'flex' }}>
+            <div>
+                <form method="get" className="card p-2 m-3" style={{ width: '250px' }}>
+                    <div style={{ display: 'flex' }}>
+                        <h5>Фильтры</h5>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="FilterMapName" className="form-label">
+                            Название карты
+                        </label>
+                        <input type="text" className="form-control" id="FilterMapName" name="FilterMapName" value="@Model.FilterMapName" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="FilterMapPrice" className="form-label">
+                            Стоимость входа
+                        </label>
+                        <input type="number" className="form-control" id="FilterMapPrice" name="FilterMapPrice" value="@Model.FilterMapPrice" min="0" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Применить
+                    </button>
+                </form>
+            </div>
+            <MapListComponent maps={maps} />
+        </div>
+    );
 };
