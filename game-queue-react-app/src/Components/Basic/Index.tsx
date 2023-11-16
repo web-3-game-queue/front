e69bd5457ef8b9db98fa06cdd4c5013d1120041c
alordash <alordash@mail.ru>
2023-11-16 20:45:30 +0300

    Added menu

--- game-queue-react-app/src/Components/Basic/Index.tsx
+++ game-queue-react-app/src/Components/Basic/Index.tsx
@@ -1,32 +1,77 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../Core/Storage/DataSlice';
+import { AuthenticationAPI } from '../../Core/APIs/AuthenticationAPI';
 
 export const IndexComponent: FC = () => {
-    const login = useAuth();
+    const auth = useAuth();
 
-    const invite =
-        login === null ? (
-            <>
-                <Link to="/Login">
-                    <h2>Войти</h2>
-                </Link>
-                <br />
-                <Link to="/register">
-                    <h2>Зарегистрироваться</h2>
-                </Link>
-            </>
-        ) : (
-            <Link to="/maps">
-                <h2>К картам</h2>
-            </Link>
+    const [isAdm, setIsAdm] = useState(false);
+
+    useEffect(() => {
+        async function checkIfLogged() {
+            const isAdm = await AuthenticationAPI.IsAdmin();
+            setIsAdm(isAdm);
+        }
+        checkIfLogged();
+    });
+
+    let menuList = (
+        <>
+            <li className="list-group-item">
+                <h2><Link to="/login">Войти</Link></h2>
+            </li>
+            <li className="list-group-item">
+                <h2><Link to="/login">Зарегистрироваться</Link></h2>
+            </li>
+        </>
+    );
+
+    if (auth !== null) {
+        const maps = (
+            <li className="list-group-item">
+                <Link to="/maps">К картам</Link>
+            </li>
         );
+        const selectedMaps = (
+            <li className="list-group-item">
+                <Link to="/cart">Выбранные карты</Link>
+            </li>
+        );
+        const requestsList = (
+            <li className="list-group-item">
+                <Link to="/requests">Список заявок</Link>
+            </li>
+        );
+        const addMap = isAdm ? (
+            <li className="list-group-item">
+                <Link to="/maps/create">Создать карту</Link>
+            </li>
+        ) : null;
+        const mapsTable = isAdm ? (
+            <li className="list-group-item">
+                <Link to="/maps/table">Таблица карт</Link>
+            </li>
+        ) : null;
+        menuList = (
+            <>
+                {maps}
+                {selectedMaps}
+                {requestsList}
+                {addMap}
+                {mapsTable}
+            </>
+        );
+    }
 
     return (
-        <>
-            {/* Well, hello there
-            <br /> */}
-            {invite}
-        </>
+        <div style={{ textAlign: '-moz-center' }}>
+            <ul className="list-group" style={{ width: 'fit-content' }}>
+                <li className="list-group-item list-group-item-primary" aria-current="true">
+                    Меню
+                </li>
+                {menuList}
+            </ul>
+        </div>
     );
 };
